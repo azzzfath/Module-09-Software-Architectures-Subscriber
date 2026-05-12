2f572299b1dc17c071415199a904a1457c87f20b
azzzfath <AzzamFathurrahman23@gmail.com>
2026-05-12 17:01:10 +0700

    Initial subscriber code.

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use crosstown_bus::{CrosstownBus, MessageHandler, HandleError};
+use std::{thread, time};
+
+#[derive(Debug, Clone, BorshDeserialize, BorshSerialize)]
+pub struct UserCreatedEventMessage {
+    pub user_id: String,
+    pub user_name: String
+}
+
+pub struct UserCreatedHandler;
+
+impl MessageHandler<UserCreatedEventMessage> for UserCreatedHandler {
+    fn handle(&self, message: Box<UserCreatedEventMessage>) -> Result<(), HandleError> {
+        let ten_millis = time::Duration::from_millis(1000);
+        let now = time::Instant::now();
+        // thread::sleep(ten_millis);
+        println!("In Azzam's Computer [2406412152]. Message received: {:?}", message);
+        Ok(())
+    }
+
+    // Tambahkan method ini untuk mengatasi Error E0046
+    fn get_handler_action(&self) -> String {
+        todo!()
+    }
+}
+
+fn main() {
+    let listener = CrosstownBus::new_queue_listener("amqp://guest:guest@localhost:5672".to_owned()).unwrap();
+    let _ = listener.listen("user_created".to_owned(), UserCreatedHandler{}, crosstown_bus::QueueProperties {
+        auto_delete: false,
+        durable: false,
+        use_dead_letter: true
+    });
+
+    loop {
+    }
+}
